f7f276ea6aeb08c261edbabd0f9acbdd8c769cf5
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 19:54:01 +0300

    feat: add write stream task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,21 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { stdin } from 'process';
+
 export const write = async () => {
     // Write your code here 
+
+    const PATH_TO_WRITE = './src/streams/files/fileToWrite.txt';
+
+    const writeStream = createWriteStream(PATH_TO_WRITE);
+
+    pipeline(
+        stdin,
+        writeStream,
+        (err) => {
+            console.log(err);
+        }
+    );
 };
+
+write();
