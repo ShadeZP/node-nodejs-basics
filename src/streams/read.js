bc5e31c958d4a60c8a8123cc215a1b14ee35dd12
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 19:47:43 +0300

    feat: add read stram task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,22 @@
+import { stdout } from 'process';
+import { pipeline } from 'stream';
+import { open } from 'fs/promises';
+
 export const read = async () => {
     // Write your code here 
+
+    const PATH = './src/streams/files/fileToRead.txt';
+
+    const fd = await open(PATH);
+    const readStream = fd.createReadStream();
+
+    pipeline(
+        readStream,
+        stdout,
+        (err) => {
+            console.log(err);
+        }
+    );
 };
+
+read();
