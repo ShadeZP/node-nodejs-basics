365105f31f334985bb2ef4e9e1a26f4a6dd3fb1b
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 20:02:45 +0300

    feat: add transform stream task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,27 @@
+import { stdout, stdin } from 'process';
+import { pipeline, Transform } from 'stream';
+
 export const transform = async () => {
     // Write your code here 
+
+    const myTransform = new Transform({
+        transform(chunk, encoding, callback) {
+          const str = chunk.toString().trim();
+          const reversedStr = str.split('').reverse().join('');
+
+          callback(null, `${reversedStr}\n`);
+        }
+      });
+    
+
+    pipeline(
+        stdin,
+        myTransform,
+        stdout,
+        (err) => {
+            console.log(err);
+        }
+    );
 };
+
+transform();
