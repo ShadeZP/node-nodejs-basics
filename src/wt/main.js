af26ccc1a0e8bfd92012a9d07d8634ee77da210e
Evgeny Vasilko <shadezp@gmail.com>
2022-06-05 15:06:39 +0300

    feat: add wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,51 @@
+import { Worker } from 'worker_threads'
+import { cpus } from 'os'
+
 export const performCalculations = async () => {
     // Write your code here
+
+    function runService(workerData) {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker('./src/wt/worker.js', { workerData });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', (code) => {
+            if (code !== 0)
+                reject(new Error(`Worker stopped with exit code ${code}`));
+            })
+        })
+      }
+      
+    async function run(i) {
+        const res = {
+            status: null,
+            data: null,
+        }
+
+        try {
+            const data = await runService(i);
+            res.status = 'resolved';
+            res.data = data;
+        } catch {
+            res.status = 'error';
+            res.data = null;
+        }
+
+        return res;
+    }
+      
+      
+    function createRuns() {
+        const cpusNum = cpus().length;
+        const promiseArray = [];
+        for (let i = 10 ; i < cpusNum + 10; i++) {
+            promiseArray.push(run(i).catch(err => console.error(err)))
+        }
+
+        return promiseArray
+    }
+      
+    Promise.all(createRuns()).then((e) => console.log(e))
 };
+
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,14 @@
+import { parentPort, workerData } from 'worker_threads'
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
-};
+    const index = workerData
+
+    const fibNum = nthFibonacci(index)
+    
+    parentPort.postMessage(fibNum)
+}
+
+sendResult();
